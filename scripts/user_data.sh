8e748b592e815a4dc7e424b4f0fa61cb8c76e7e9
Sifa Collins <109460248+sifakasena@users.noreply.github.com>
2025-07-10 09:44:07 +0000

    Daemonize flask with gunicorn

---
+++ scripts/user_data.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Update and install dependencies
+sudo yum update -y
+sudo yum install -y python3 python3-pip nginx git
+
+# Change working directcory
+cd $HOME
+
+# Clone repo
+git clone https://github.com/SifaKasena/aws-hello-world.git
+
+# Setup virtual environment
+cd aws-hello-world
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements
+deactivate
+
+# Daemonize gunicorn
+sudo cp scripts/flask-api.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start flask-api
+sudo systemctl enable flask-api
+
+# Clean up
+cd -
+rm -rf !(flask_api)
